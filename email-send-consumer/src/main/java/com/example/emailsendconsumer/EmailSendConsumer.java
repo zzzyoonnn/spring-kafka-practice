ff3e646b9b079c193c2b75413e10071fc3c9c13c
zzzyoonnn <dwsrh03@gmail.com>
2026-06-02 01:27:08 +0900

    feat: implement Kafka email consumer

---
+++ email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
@@ -0,0 +1,21 @@
+package com.example.emailsendconsumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSendConsumer {
+
+  @KafkaListener(
+          topics = "email.send",
+          groupId = "email-send-group"
+  )
+  public void consume(String message) {
+    System.out.println("kafka로부터 받아온 메시지: " + message);
+
+    EmailSendMessage emailSendMessage = EmailSendMessage.fromJson(message);
+
+    // 실제 이메일 발송 로직 생략
+    System.out.println("이메일 발송 완료");
+  }
+}
---
+++ email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendMessage.java
@@ -0,0 +1,43 @@
+package com.example.emailsendconsumer;
+
+import tools.jackson.databind.ObjectMapper;
+
+public class EmailSendMessage {
+
+  private String from;
+  private String to;
+  private String subject;
+  private String body;
+
+  public EmailSendMessage() {
+
+  }
+
+  public EmailSendMessage(String from, String to, String subject, String body) {
+    this.from = from;
+    this.to = to;
+    this.subject = subject;
+    this.body = body;
+  }
+
+  public static EmailSendMessage fromJson(String json) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    return objectMapper.readValue(json, EmailSendMessage.class);
+  }
+
+  public String getFrom() {
+    return from;
+  }
+
+  public String getTo() {
+    return to;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public String getBody() {
+    return body;
+  }
+}
